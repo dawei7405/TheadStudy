f3b62b2cceb78be9589fad40a4a16a04c4548627
dawei7405 <872894588@qq.com>
2022-07-19 22:36:00 +0800

    commit test

--- src/main/java/com/dawei/theadstudy/TheadStudyApplication.java
+++ src/main/java/com/dawei/theadstudy/TheadStudyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TheadStudyApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(TheadStudyApplication.class, args);
     }
